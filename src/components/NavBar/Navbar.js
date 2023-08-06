71191c819b441ea5d2d5f25d9dbc3c02f7402990
Harcole <hamees.ehsan@gmail.com>
2023-08-06 22:09:26 +0500

    Added animations

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,12 +1,12 @@
-
 import './Navbar.css'
-
+import {LiaSearchSolid} from 'react-icons/lia'
 function Navbar() {
   
   return (
   <div className='nav_container'>
      <nav className='nav_bar'> 
-     <form action="https://www.google.com/search" method="get">
+     <form action="https://www.google.com/search" method="get" className='searchForm'>
+        <LiaSearchSolid size={25} className='search_icon' fill='cyan'/>
         <input className="search_bar" name="q" type="text" placeholder=' Google Search! '/>
      </form>
     </nav> 
